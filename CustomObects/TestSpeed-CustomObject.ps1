fccc3c07eb6bfbd0796e32d06df7bf5f78cffad3
brent09 <brentdenny@gmail.com>
2018-02-24 11:49:15 +1000

    force login by test change

---
+++ CustomObects/TestSpeed-CustomObject.ps1
@@ -0,0 +1,15 @@
+﻿$MaxNum = 5000
+
+Write-Host -Foreground Yellow "Time to enumerate a $MaxNum element object collection using [PSObject]"
+Measure-Command -Expression  {
+  (0..$MaxNum) | ForEach-Object {
+    [psobject]@{Name = "Test Name"; ID = $_}
+  } 
+} | Format-Table TotalSeconds -Autosize
+
+Write-Host -Foreground Yellow "Time to enumerate a $MaxNum element object collection using [PSCustomObject]"
+Measure-Command -Expression {
+  (0..$MaxNum) | ForEach-Object {
+    [pscustomobject]@{Name = "Test Name"; ID =$_}
+  }
+} | Format-Table TotalSeconds -Autosize
