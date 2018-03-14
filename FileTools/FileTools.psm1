8d70ed68e6f33407e599628584b72080f5767184
brentd09 <brentdenny@gmail.com>
2018-03-15 04:41:11 +1000

    added the filetools module

---
+++ FileTools/FileTools.psm1
@@ -0,0 +1,15 @@
+﻿function Touch-File {
+  [cmdletbinding()]
+  param (
+    [Parameter(Mandatory=$true)]
+    [string[]]$Filename
+  )
+  foreach ($file in $Filename ) {
+    if (Test-Path $file) {
+      (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else {
+      $null > $file 
+    }
+  }
+}
