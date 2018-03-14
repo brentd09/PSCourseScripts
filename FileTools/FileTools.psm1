3465efe3209b5d7b4aa40ea86c486cfc7dff24ac
brentd09 <brentdenny@gmail.com>
2018-03-15 05:01:07 +1000

    Competed the Touch-File function

--- FileTools/FileTools.psm1
+++ FileTools/FileTools.psm1
@@ -1,4 +1,24 @@
 ﻿function Touch-File {
+<#
+.Synopsis
+   UNIX touch 
+.DESCRIPTION
+   This does the same function as a unix touch command in that any filename listed
+   in the -filename parameter will have its LastWriteTime updated to current time 
+   or if the file does not exist an empty file will be created.
+.EXAMPLE
+   Touch-File -FileName test01.tmp,test02a.tmp,fwaw.log
+
+   This example updates or creates all of these files to the current directory
+.EXAMPLE
+   Touch-File -FileName test01.tmp
+
+   This eample only creates or updates this one file
+.NOTES
+   General notes
+   Created by : Brent Denny
+   Created on : 15 Mar 2018
+#>
   [cmdletbinding()]
   param (
     [Parameter(Mandatory=$true)]
@@ -6,10 +26,13 @@
   )
   foreach ($file in $Filename ) {
     if (Test-Path $file) {
-      (Get-ChildItem $file).LastWriteTime = Get-Date
-    }
+      try {(Get-ChildItem $file).LastWriteTime = Get-Date -ErrorAction Stop} 
+      Catch {Write-Warning "Problem updating file $file"}   
+      
+    } # end if
     else {
-      $null > $file 
-    }
-  }
-}
+      try {$null | out-file -Append -FilePath $file -ErrorAction stop} 
+      Catch {Write-Warning "Problem writing to file $file"}   
+    } # end else
+  } # end foreach
+} #end function
