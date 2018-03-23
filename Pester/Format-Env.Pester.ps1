4f989e7305fc604dd1a63d39e836c61f73adbe27
brentd09 <brentdenny@gmail.com>
2018-03-24 07:14:37 +1000

    added another example pester

---
+++ Pester/Format-Env.Pester.ps1
@@ -0,0 +1,12 @@
+﻿Describe {
+  It "Converts '<value' to '<expected>'" -TestCases @(
+    @{Value = 'BigData';Expected = 'Big_Data'}
+    @{Value = 'ABC';Expected = 'A_B_C'}
+  ) {
+    Param ($Value,$Expected)
+
+    Format-Env $Value |
+      should Be $Expected
+  }
+
+}
