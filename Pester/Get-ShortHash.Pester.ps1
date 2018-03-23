5e1c27fe6af0db55ac4ea0f898f39e15902d2330
brentd09 <brentdenny@gmail.com>
2018-03-24 07:02:42 +1000

    added Pester example

---
+++ Pester/Get-ShortHash.Pester.ps1
@@ -0,0 +1,9 @@
+﻿
+Describe "Get-ShortHash" {
+  It 'Converts a long hash to a 7 character hash' {
+    $Hash = 'ca146ab4231afed311de6423aa23'
+    Get-ShortHash -Hash $Hash |
+      should Be 'ca146ab'
+  }
+
+}
---
+++ Pester/HashTools.psm1
@@ -0,0 +1,16 @@
+﻿function Get-ShortHash {
+  Param ( $Hash )
+
+  # Youtube video https://www.youtube.com/watch?v=F3oOk0BC9B4
+  # For Demo uncomment one line only and then do the unit testing by running the 
+  # Pester script, make sure you remove-module and import-module between tests
+  # Only the last line of code will give the expected (Asserted) result.
+
+#  First attempt
+#  $Hash[1..5]
+#  Next attempt
+#  $Hash[0..6]
+#  Final attempt
+#  -join $Hash[0..6]       
+
+}
