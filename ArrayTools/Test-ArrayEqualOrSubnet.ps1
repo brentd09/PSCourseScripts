eeb205c1c44228b0628b9e8e85710313de3aaf1b
Brent Denny <brentdenny@gmail.com>
2018-04-10 10:54:40 +1000

    Added a script to test array equal or subset or not equal

---
+++ ArrayTools/Test-ArrayEqualOrSubnet.ps1
@@ -0,0 +1,11 @@
+﻿$A = 1,2,3,5
+$B = 1,2,3,4
+
+$AB = $A | where {$_ -notin $B}
+$BA = $B | where {$_ -notin $A}
+
+if     ($AB -eq $null -and $BA -eq $null) {'$A and $B arrays are the same'}
+elseif ($AB -eq $null -and $BA -ne $null) {'$A is a subset of $B'}
+elseif ($AB -ne $null -and $BA -eq $null) {'$B is a subset of $A'}
+elseif ($AB -ne $null -and $BA -ne $null) {'$A and $B are not equal or subsets of each other'}
+
