edc07df5e14c8c4266343232c8f920b1788b406d
brent denny <brentdenny@gmail.com>
2018-09-08 06:08:22 +1000

    Cleaned up the API  code

--- API/apiExamples.psm1
+++ API/apiExamples.psm1
@@ -9,50 +9,56 @@
      If there is more than one city in the database the script will show 
      all known cities and their details.
   
-     Coordinate Accuracy
-     places   degrees          distance
-     -------  -------          --------
-     0        1                111  km
-     1        0.1              11.1 km
-     2        0.01             1.11 km
-     3        0.001            111  m
-     4        0.0001           11.1 m
-     5        0.00001          1.11 m
-     6        0.000001         11.1 cm
-     7        0.0000001        1.11 cm
-     8        0.00000001       1.11 mm
+     Latitude and Longitude Coordinate Accuracy
+     Decimal Places   Example          Error Tollerence
+     --------------   -------          ----------------
+     0                1                111  km
+     1                0.1              11.1 km
+     2                0.01             1.11 km
+     3                0.001            111  m
+     4                0.0001           11.1 m
+     5                0.00001          1.11 m
+     6                0.000001         11.1 cm
+     7                0.0000001        1.11 cm
+     8                0.00000001       1.11 mm
   .EXAMPLE
-     ConvertTo-LatLngCoords -City Chicago
+     ConvertTo-LatLngCoords -City Melbourne
+     This will expect the API to make the best guess as to which Melbourne
+     you wanted to find, if it chose the wrong one you will need to refine
+     your request with state and/or region information.
   .EXAMPLE
-     ConvertTo-LatLngCoords -City Chicago,Sydney
+     ConvertTo-LatLngCoords -City Melbourne, Florida
+     You can further refine where the city is by adding a state or region.
+  .EXAMPLE
+     ConvertTo-LatLngCoords -City "1600 Pennsylvania Avenue, Washington DC"
+     You can further refine this to an address, for example the White House
+     address in Washington DC
   .PARAMETER city
      Choose any major or semi-major city in the world to be converted into 
-     Coordicates
+     Coordicates. You can give as much detail as you wish, just seperate the
+     street, city, state, country by commas.
   .NOTES
      General notes
      Creation: Brent Denny 11/Oct/2017
      Modified: 1 Dec 2017
    .ROLE
      The role this cmdlet is to show how APIs work and how PowerShell
-     can use them
+     can use them.
   #>
   [CmdletBinding()]
   Param(
     [parameter(Mandatory=$true)]
     [string[]]$City
   )
-  
-  foreach ($CityName in $City) {  # Use  Google API to get GEOCODE
-    $resultAPI = Invoke-RestMethod -Method Get -Uri "https://maps.googleapis.com/maps/api/geocode/json?address=${CityName}&sensor=false" -UseBasicParsing
-    $locations = $resultAPI.results
-    foreach ($location in $locations) {  # Just in case there are multiple cities with same name
-      $Coords = $location.geometry.location
-      $outputProp = [ordered]@{
-        Lat = ('{0:N6}' -f $Coords.lat) -as [double]
-        Lng = ('{0:N6}' -f $Coords.lng) -as [double]
-        City = $location.formatted_address
-      } # END Hashtable
-      New-Object -TypeName psobject -Property $outputProp
-    } #END Foreach
+  $resultAPI = Invoke-RestMethod -Method Get -Uri "https://maps.googleapis.com/maps/api/geocode/json?address=${City}&sensor=false" -UseBasicParsing
+  $locations = $resultAPI.results
+  foreach ($location in $locations) {  # Just in case there are multiple cities with same name
+    $Coords = $location.geometry.location
+    $outputProp = [ordered]@{
+      Lat = ('{0:N6}' -f $Coords.lat) -as [double]
+      Lng = ('{0:N6}' -f $Coords.lng) -as [double]
+      City = $location.formatted_address
+    } # END Hashtable
+    New-Object -TypeName psobject -Property $outputProp
   } #END Foreach
 } #END Function
