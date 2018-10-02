79d64f03a68bb909434bd27a64373940a9abfbb5
Grant Carthew <grant.carthew@ddls.com.au>
2018-10-03 08:51:08 +1000

    Setup a DSC demo for PS courses

---
+++ DSCDemo/mydemos/DSCDemo/DSCConfig.ps1
@@ -0,0 +1,19 @@
+﻿Configuration WebIISDeploy {
+
+  Node 'LON-SVR1' {
+    WindowsFeature WebServer {
+      Ensure = 'Present'
+      Name = 'Web-Server'
+    } # Windows Feature
+
+    File WebContent {
+      Ensure = 'Present'
+      SourcePath = '\\lon-cl1\webdemo\'
+      DestinationPath = 'c:\Inetpub\wwwroot\'
+      DependsOn = '[WindowsFeature]WebServer'
+      Force = $true
+      Recurse = $true
+    } # File
+  } # Node
+} # Configuration
+
