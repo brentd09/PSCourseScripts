628d1b17b1a6d73d6d39aa61ab8bb1a76621d56e
Brent Denny <brent.denny@ddls.com.au>
2018-10-31 08:07:04 +1000

    Fixed the Json example

--- JSON/WorkingWithJSON.ps1
+++ JSON/WorkingWithJSON.ps1
@@ -1,4 +1,4 @@
-﻿$json = @'
+﻿$JSONString = @'
 {"employees":[
     { "firstName":"John",
       "lastName":"Doe" 
@@ -12,6 +12,11 @@
 ]}
 '@
 
-$PSArray = $json | ConvertFrom-Json
-$PSArray.employees += ($PSArray.employees | Select-Object @{n='Email';e={($_.firstName).tolower()+'.'+($_.lastName).tolower()+'@adatum.com'}})
-$PSArray
+# This code converts the JSON into an array of PowerShell Objects
+# we then add a calcualted property Email
+# this is then injected back into the object array
+# lastly the array is converted back to JSON
+$CompanyObj = $JSONString | ConvertFrom-Json
+$EmployeesAndEmail = $CompanyObj.employees | Select-Object *,@{n='Email';e={($_.firstName).tolower()+'.'+($_.lastName).tolower()+'@adatum.com'}}
+$CompanyObj.employees = $EmployeesAndEmail
+$CompanyObj | ConvertTo-Json
