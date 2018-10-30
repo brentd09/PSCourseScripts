29bdff62a6d92392d4a6eb472d4fefd29a798ec1
Brent Denny <brent.denny@ddls.com.au>
2018-10-30 15:46:53 +1000

    Fixed the XML demo

--- XML/WorkWithXML.ps1
+++ XML/WorkWithXML.ps1
@@ -1,42 +1,44 @@
-﻿[xml]$Books = get-content .\Books-sample.xml
+﻿[xml]$Catalog = get-content .\Books-sample.xml
 
 ### XPATH QUERIES
 # / - search from current position in the DOM
-$books.SelectNodes("/catalog/book[price=36.95]")
-$books.SelectNodes("/catalog/book[author='Galos, Mike']")
+$Catalog.SelectNodes("/catalog/book[price]")
+$Catalog.SelectNodes("/catalog/book[author='Galos, Mike']")
 
-# // search from the root of the DOM
-$parent = $Books.SelectSingleNode('//catalog')
-$child = $books.CreateElement('element','book','')
+# Create new Book
+$parent = $Catalog.SelectSingleNode('/catalog')
+$child = $Catalog.CreateElement('element','book','')
 $parent.AppendChild($child)
 
-# Find the last book from the XML data
-$parent = $Books.SelectSingleNode('/catalog/book[last()]')  
-$id = $books.CreateElement('element','id','')
-$author = $books.CreateElement('element','author','')
-$title = $books.CreateElement('element','title','')
-$genre = $books.CreateElement('element','genre','')
-$price = $books.CreateElement('element','price','')
-$PubDate = $books.CreateElement('element','published_date','')
-$Desc = $books.CreateElement('element','description','')
+# Find New Book
+$NewBook = $Catalog.SelectSingleNode('/catalog/book[last()]')  
 
-# Append a new book structure to the end of the XML
-$parent.AppendChild($id)
-$parent.AppendChild($author)
-$parent.AppendChild($title)
-$parent.AppendChild($genre)
-$parent.AppendChild($price)
-$parent.AppendChild($PubDate)
-$parent.AppendChild($Desc)
+# Add structure 
+$id      = $Catalog.CreateElement('element','id','')
+$author  = $Catalog.CreateElement('element','author','')
+$title   = $Catalog.CreateElement('element','title','')
+$genre   = $Catalog.CreateElement('element','genre','')
+$price   = $Catalog.CreateElement('element','price','')
+$PubDate = $Catalog.CreateElement('element','publish_date','')
+$Desc    = $Catalog.CreateElement('element','description','')
+
+# Append a strucure to the new book
+$NewBook.AppendChild($id)
+$NewBook.AppendChild($author)
+$NewBook.AppendChild($title)
+$NewBook.AppendChild($genre)
+$NewBook.AppendChild($price)
+$NewBook.AppendChild($PubDate)
+$NewBook.AppendChild($Desc)
 
 # Populate the last empty book info with data
-$Books.SelectSingleNode('/catalog/book[last()]').id = 'bk113'
-$Books.SelectSingleNode('/catalog/book[last()]').author = 'Denny, Brent'
-$Books.SelectSingleNode('/catalog/book[last()]').title = 'The Life of a scripter'
-$Books.SelectSingleNode('/catalog/book[last()]').genre = 'IT'
-$Books.SelectSingleNode('/catalog/book[last()]').price = '10.34'
-$Books.SelectSingleNode('/catalog/book[last()]').published_date = '2018-01-22'
-$Books.SelectSingleNode('/catalog/book[last()]').Description = 'A really boring book'
+$Catalog.SelectSingleNode('/catalog/book[last()]').id = 'bk113'
+$Catalog.SelectSingleNode('/catalog/book[last()]').author = 'Denny, Brent'
+$Catalog.SelectSingleNode('/catalog/book[last()]').title = 'The Life of a scripter'
+$Catalog.SelectSingleNode('/catalog/book[last()]').genre = 'IT'
+$Catalog.SelectSingleNode('/catalog/book[last()]').price = '10.34'
+$Catalog.SelectSingleNode('/catalog/book[last()]').publish_date = '2018-01-22'
+$Catalog.SelectSingleNode('/catalog/book[last()]').Description = 'A really boring book'
 
 # Save the modified XML data to a file
-$books.Save("C:\Users\Administrator\Documents\newbooks.xml")
+$Catalog.Save("C:\Users\Administrator\Documents\newbooks.xml")
