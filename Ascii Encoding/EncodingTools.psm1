53c927979ddb408aeeda4ee966709ccf02423926
Brent Denny <brent.denny@ddls.com.au>
2019-02-21 13:25:55 +1100

    fixed 2 typos

--- Ascii Encoding/EncodingTools.psm1
+++ Ascii Encoding/EncodingTools.psm1
@@ -7,7 +7,7 @@ function Convertto-Base64 {
   $Base64 = [System.Convert]::ToBase64String($Bytes)
   $ObjProp = [ordered]@{
     Text = $TextToConvert
-    Base64 = $Base64String
+    Base64 = $Base64
   }
   New-Object -TypeName psobject -Property $ObjProp
 }
@@ -15,7 +15,7 @@ function Convertto-Base64 {
 function ConvertFrom-Base64 {
   Param (
     [Parameter(Mandatory=$true)]
-    [string]$Bas64String
+    [string]$Base64String
   )
   $Bytes = [System.Convert]::FromBase64String($Bas64String)
   $EncodingType = [System.Text.Encoding]::ASCII
