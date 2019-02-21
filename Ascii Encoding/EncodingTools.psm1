131d8805ade97d36d813b2cbd2ef1d5d60bfe906
Brent Denny <brent.denny@ddls.com.au>
2019-02-21 13:16:20 +1100

    B64 encoding tools

---
+++ Ascii Encoding/EncodingTools.psm1
@@ -0,0 +1,28 @@
+function Convertto-Base64 {
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string]$TextToConvert
+  )
+  $Bytes = [System.Text.Encoding]::UTF8.GetBytes($TextToConvert)
+  $Base64 = [System.Convert]::ToBase64String($Bytes)
+  $ObjProp = [ordered]@{
+    Text = $TextToConvert
+    Base64 = $Base64
+  }
+  New-Object -TypeName psobject -Property $ObjProp
+}
+
+function ConvertFrom-Base64 {
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string]$Bas64String
+  )
+  $Bytes = [System.Convert]::FromBase64String($Bas64String)
+  $EncodingType = [System.Text.Encoding]::ASCII
+  $ASCIIString = $EncodingType.GetString($Bytes)
+  $ObjProp = [ordered]@{
+    Text = $ASCIIString
+    Base64 = $Base64String
+  }
+  New-Object -TypeName psobject -Property $ObjProp
+}
