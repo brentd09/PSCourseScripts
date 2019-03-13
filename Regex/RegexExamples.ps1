bca6897d2b769a2f4b8a62fea196a79e63f5643d
Brent Denny <brent.denny@ddls.com.au>
2019-03-13 12:54:46 +1000

    Changes to regex examples

--- Regex/RegexExamples.ps1
+++ Regex/RegexExamples.ps1
@@ -1,22 +1,24 @@
-#Entering Cell phone info
-$CellPhone = Read-Host -Prompt "Please enter your cell phone number"
-#Check if Cell number was entered corrently 
-#Numbers can be entered as 10 digits with or with out spaces (0432 213 123)
-# OR
-#Numbers can be entered with the country code eliminating the first number (+61 123 345 456)
-if ($CellPhone -match "^(?:(?:\+(?:\d\s*){1,3}(?:\s*\d){9})|(?:(?:\s*\d){10}))$") {
-  "Your number was entered correctly, now we will format it to our standard layout"
-  if ($CellPhone -match "^(?:\d\s*){10}$") {
-    $ProperlyFormatted = $CellPhone -replace "\s*",'' -replace "^((?:\d\s*){3})((?:\d\s*){4})((?:\d\s*){3})$",'$1 $2 $3'
+Class MobilePhone {
+  [string]$Number
+
+  MobilePhone ([string]$CellNumber) {
+    if ($CellNumber -match "^(?:(?:\+(?:\d\s*){1,3}(?:\s*\d){9})|(?:(?:\s*\d){10}))$") {
+      if ($CellNumber -match "^(?:\d\s*){10}$") {
+        [string]$ProperlyFormatted = $CellNumber -replace "\s*",'' -replace "^((?:\d\s*){3})((?:\d\s*){4})((?:\d\s*){3})$",'$1 $2 $3'
+      }
+      elseif ($CellNumber -match "^\+(?:\d\s*){1,3}(?:\d\s*){9}") {
+        $PlainNumber = $ProperlyFormatted = $CellNumber -replace "\s*",'' 
+        $NumberSize = $PlainNumber.Length
+        $CCodeLength = $NumberSize - 9
+        $CCode = $PlainNumber.Substring(0,$CCodeLength)
+        $PhoneNoCC = $PlainNumber.Substring($CCodeLength,$NumberSize-$CCodeLength)
+        [string]$ProperlyFormatted = $CCode + " " + ($PhoneNoCC -replace "(\d{3})(\d{3})(\d{3})",'$1 $2 $3')
+      }
+      $this.Number = $ProperlyFormatted
+    }
+    else {throw "The number entered does not conform to Australian mobile standards"}
   }
-  elseif ($CellPhone -match "^\+(?:\d\s*){1,3}(?:\d\s*){9}") {
-    $PlainNumber = $ProperlyFormatted = $CellPhone -replace "\s*",'' 
-    $NumberSize = $PlainNumber.Length
-    $CCodeLength = $NumberSize - 9
-    $CCode = $PlainNumber.Substring(0,$CCodeLength)
-    $PhoneNoCC = $PlainNumber.Substring($CCodeLength,$NumberSize-$CCodeLength)
-    $ProperlyFormatted = $PlainNumber -replace "^(\w{\$CCodeLength})(\d{3})(\d{3})(\d{3})$",'$1 $2 $3 $4'
-  }
-  $ProperlyFormatted
 }
-else {"You did not enter the number corectly"}
+
+$NewNumber = [MobilePhone]::New('+61432123111')
+$NewNumber
