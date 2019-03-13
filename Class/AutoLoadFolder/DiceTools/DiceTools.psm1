204e24dd0dc1ffa16ae951af8965161c055f3781
Brent Denny <brent.denny@ddls.com.au>
2019-03-13 22:41:41 +1000

    Created shared class modiule

---
+++ Class/AutoLoadFolder/DiceTools/DiceTools.psm1
@@ -0,0 +1,15 @@
+﻿USING Module DiceClass
+Function Roll-Dice {
+  Param (
+    [Parameter(Mandatory=$true)]
+    [int]$NumberOfDice,
+    [Parameter(Mandatory=$true)]
+    [int]$NumberOfSides,
+    [Parameter(Mandatory=$true)]
+    [int]$NumberOfRolls
+  )
+  $Dice = [Dice]::New($NumberOfDice,$NumberOfSides)
+  1..$NumberOfRolls | ForEach-Object {
+    $Dice.Roll() 
+  }
+}
