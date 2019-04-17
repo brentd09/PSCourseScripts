5e8dd5631ab0e9488ec2940d341f8dfb420215f3
Brent Denny <brent.denny@ddls.com.au>
2019-04-17 23:47:04 +1000

    tidy up code

--- CalcPropFormatting/FormattingCalcProperty.ps1
+++ CalcPropFormatting/FormattingCalcProperty.ps1
@@ -6,7 +6,7 @@
 get-volume | 
   Where-Object {$_.DriveType -eq "Fixed"} |
   Format-Table -Property  DriveLetter,
-                        @{n="SpaceUsed(GB)";e={"{0:N2}" -f (($_.size - $_.sizeremaining) / 1GB)};a="right"}
+                        @{n="SpaceUsed(GB)";e={"{0:N2}" -f (($_.size - $_.sizeremaining) / 1GB)};align="right"}
 
 get-volume | 
   Where-Object {$_.DriveType -eq "Fixed"} |
