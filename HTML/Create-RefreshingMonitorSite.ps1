cfbc7af7aca11c9f36c377950726571fb0023128
Brent Denny <brent.denny@ddls.com.au>
2019-04-17 17:50:54 +1000

    Created a monitored self refreshing HTML from PS

---
+++ HTML/Create-RefreshingMonitorSite.ps1
@@ -0,0 +1,18 @@
+$trig = New-JobTrigger -RepetitionInterval 0:01:00 -RepeatIndefinitely -at 00:00 -once
+ Register-ScheduledJob -Name report -Trigger $trig -ScriptBlock {
+$CSS = @'
+<style>
+  body {background-color:rgba(86, 86, 86,.4)}
+  table, th, td, tr {border:solid;border-collapse:collapse;width:50%;border-color:black}
+  th {background-color:LightGray;}
+  .Stopped {background-color:rgba(193, 0, 0,1);color:white;}
+  .Running {background-color:rgba(0, 135, 51,.3);}
+</style>
+'@
+
+$HTMLDoc= Get-Service -Name Browser,LanmanServer,LanmanWorkstation,Audiosrv,BITS,Dhcp,Dnscache,Netlogon,Spooler,W32Time,wuauserv | 
+  Select-Object Status,StartType,Name |
+  ConvertTo-Html -Head "<meta http-equiv='refresh' content='10'>$CSS" -PreContent "<h1>Services</h1>" 
+$OutDoc = $HTMLDoc -replace "<tr><td>(\w+)</td>", '<tr class="$1"><td>$1</td>' 
+$OutDoc | Out-File C:\inetpub\wwwroot\index.html
+}
