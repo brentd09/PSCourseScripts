9c69bac55cfb9da71b1b5ac1a6de0dbcdc2518e2
Brent Denny <brent.denny@ddls.com.au>
2019-04-18 12:19:25 +1000

    cleaning up the code

--- HTML/Create-RefreshingMonitorSite.ps1
+++ HTML/Create-RefreshingMonitorSite.ps1
@@ -1,5 +1,38 @@
-# $trig = New-JobTrigger -RepetitionInterval 0:01:00 -RepeatIndefinitely -at 09:13 -once
-# Register-ScheduledJob -Name report -Trigger $trig -FilePath c:\Create-RefreshingMonitorSite.ps1
+<#
+.SYNOPSIS
+  This creates a website the monitors services
+.DESCRIPTION
+  This script monitors services and then creates a HTML doc based on the service statuses.
+  The web page automatically refreshes every 10 seconds. For this to work however there
+  needs to be a scheduled job created that will re-run the PowerShell script every minute 
+  to refresh the web document.
+.EXAMPLE
+  PS C:\> <example usage>
+  Explanation of what the example does
+.INPUTS
+  Inputs (if any)
+.OUTPUTS
+  Output (if any)
+.NOTES
+  General notes
+#>
+[CmdletBinding()]
+Param (
+  [string]$ScriptPath = 'C:\inetpub\wwwroot\reporting\Create-RefreshingMonitorSite.ps1'
+)
+
+$SchedJobRunning = Get-ScheduledJob | Where-Object {$_.name -eq 'RefreshingMonitorSite'} 
+if ($SchedJobRunning.count -eq 0) {
+  try {
+    Test-Path $ScriptPath -ErrorAction Stop 
+  }
+  Catch {
+    Write-Warning "The script path ($ScriptPath) is not valid"
+    break
+  }
+  $trig = New-JobTrigger -RepetitionInterval 0:01:00 -RepeatIndefinitely -at 09:13 -once
+  Register-ScheduledJob -Name RefreshingMonitorSite -Trigger $trig -FilePath $ScriptPath
+}
 $Opac = .8
 $CSS = @"
 <style>
@@ -19,6 +52,6 @@ $HTMLDoc= Get-Service |
   Select-Object Status,Name,StartType |
   Sort-Object -Property Status,StartType,Name |
   ConvertTo-Html -Head "<meta http-equiv='refresh' content='10'>$CSS" -PreContent "<h1>Service Monitoring</h1>" 
-$OutDoc = $HTMLDoc -replace "<tr><td>(\w+)</td><td>(\w+)</td><td>(\w+)</td></tr>",'<tr class="$1$3"><td>$1</td><td>$2</td><td>$3</td></tr>' 
+$OutDoc = $HTMLDoc -replace "<tr><td>(.+)</td><td>(.+)</td><td>(.+)</td></tr>",'<tr class="$1$3"><td>$1</td><td>$2</td><td>$3</td></tr>' 
 
-$OutDoc | Out-File C:\inetpub\wwwroot\index.html
+$OutDoc | Out-File C:\inetpub\wwwroot\reporting\index.html -Force
