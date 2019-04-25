eeb7ba5e2916a09ab3e787cb2f4af4ec1d3d7741
Brent Denny <brent.denny@ddls.com.au>
2019-04-25 20:08:35 +1000

    changed dice class

--- Class/DiceClass.ps1
+++ Class/DiceClass.ps1
@@ -1,29 +1,17 @@
 ﻿Class Dice {
-  [int]$Count
   [int]$Faces
 
-  Dice ($DCount,$DFaces) {
-    $this.Count = $DCount
+  Dice ($DFaces) {
     $this.Faces = $DFaces
   }
 
-  [psobject]Roll () {
-    $DieFaces = @()
-    $AddedFaces = 0
-    1..$this.Count | ForEach-Object {
-      $RollValue = Get-Random -Maximum ($this.Faces + 1) -Minimum 1
-      $DieFaces += $RollValue
-      $AddedFaces = $AddedFaces + $RollValue
-    }
-    $objProp = [ordered]@{
-      Faces = $DieFaces
-      Total = $AddedFaces
-    }
-    return (New-object -TypeName psobject -Property $objProp)
+  [int]Roll () {
+    $RollValue = Get-Random -Maximum ($this.Faces + 1) -Minimum 1
+    return $RollValue
   }
 }
 
-$DiceResult = [Dice]::New(2,6)
+$DiceResult = [Dice]::New(6)
 1..10 | ForEach-Object {
   $DiceResult.Roll() 
 }
