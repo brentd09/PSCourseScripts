e6bb473dfec0e692767c639a7d7383eed5940581
Brent Denny <brent.denny@ddls.com.au>
2019-05-08 09:41:12 +1000

    Changes to pipleing

--- Pipelines/Get-PipelineMethod.ps1
+++ Pipelines/Get-PipelineMethod.ps1
@@ -10,19 +10,27 @@ Param (
 $ByVal = $false
 $ByPN  = $false
 $FirstObjectType = (Invoke-Expression $FirstCommand)[0].gettype().name
-$SecondCmdParamThatPipe = (get-help -full stop-service).parameters.parameter | 
+$SecondCmdParamThatPipe = (get-help -full $SecondCommand).parameters.parameter | 
   Select-Object -Property Name,PipelineInput,type | 
   Where-Object {$_.pipelineinput -like '*byValue*'}
-foreach ($TypeName in $SecondCmdParamThatPipe.Type.Name) {
-  if ($TypeName -match $FirstObjectType) {$ByVal = $true; break}
+foreach ($TypeName in $SecondCmdParamThatPipe) {
+  $pipeingParam = $TypeName.Name
+  if ($TypeName.type.name -match $FirstObjectType) {$ByVal = $true; break}
 }
+# Try ByValue logic first
 If ($ByVal -eq $true) {
   Write-host -ForegroundColor Yellow -NoNewline "$FirstCommand `| $SecondCommand "
   Write-Host -NoNewline "will pipeline data "
   Write-Host -ForegroundColor green "ByValue"
+  Write-Host -NoNewline "The first command created an object type of "
+  Write-Host -ForegroundColor green $FirstObjectType
+  Write-host -NoNewline "The second command can accept "
+  write-host -NoNewline -ForegroundColor Green $FirstObjectType
+  Write-Host -NoNewline " ByValue with parameter "
+  Write-Host -ForegroundColor Green $PipeingParam
 }
-
-
-
+else {
+  
+}
 
 # Test pipeline by PropertyName if Value failed
