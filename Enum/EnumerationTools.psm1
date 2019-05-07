05fa1b3d701e1b7b1533822a5f88f2a8396a07a0
Brent Denny <brent.denny@ddls.com.au>
2019-05-07 16:08:43 +1000

    enum help added

--- Enum/EnumerationTools.psm1
+++ Enum/EnumerationTools.psm1
@@ -1,7 +1,26 @@
 Function Get-Enum {
+  <#
+  .SYNOPSIS
+    Finds the enum for a .Net object
+  .DESCRIPTION
+    This script finds the enumeration for a .Net class
+  .EXAMPLE
+    Get-Enum -TypeClass '[System.ServiceProcess.ServiceControllerStatus]'
+    This will inspect the class and determine what values are associated 
+    with the names in the Enumeration
+  .PARAMETER TypeClass
+    This needs to be a string that includes the entire name of the class 
+    wrapped in [ ]
+  .NOTES
+    General notes
+    Created by: Brent Denny
+    When:       long ago
+    Added help : 7-May-2019
+  #>
+  [cmdletbinding()]
   Param (
     [Parameter(Mandatory=$true)]
-    $TypeClass
+    [string]$TypeClass
   )
   $EnumNum = [enum]::GetValues($TypeClass).Value__
   $EnumStr = [enum]::GetNames($TypeClass)
