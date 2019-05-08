3f4b27e90355eb1ff19d05dadc88c317d427eab3
Brent Denny <brent.denny@ddls.com.au>
2019-05-08 13:39:26 +1000

    pipeline now perfected for byval, just need ByPN

--- Pipelines/Get-PipelineMethod.ps1
+++ Pipelines/Get-PipelineMethod.ps1
@@ -9,28 +9,38 @@ Param (
 # Test pipeline by Value first
 $ByVal = $false
 $ByPN  = $false
-$FirstObjectType = (Invoke-Expression $FirstCommand)[0].gettype().name
-$SecondCmdParamThatPipe = (get-help -full $SecondCommand).parameters.parameter | 
+$FirstCmdObjectType = (Invoke-Expression $FirstCommand)[0].gettype().name
+$GetMemberResult = (Invoke-Expression $FirstCommand)[0] | Get-Member -MemberType Properties
+$FirstCmdProperties = $GetMemberResult | Select-Object Name, @{n='PropertyObjType';e={
+  #  This tests to see if the member is an alias if not get the type, if it is then find the property it is refering to first
+  if ($_.MemberType -notlike 'Alias*') {$_.definition -replace '^(?:\w+\.)*(\w+)\W.*$','$1'}
+  else {($GetMemberResult | Where-Object name -eq ($_.definition -replace '^.+=\W*(\w+)$','$1')) -replace '^(?:\w+\.)*(\w+)\W.*$','$1'}
+}}
+$SecondCmdParamThatPipeByVal = (get-help -full $SecondCommand).parameters.parameter | 
   Select-Object -Property Name,PipelineInput,type | 
   Where-Object {$_.pipelineinput -like '*byValue*'}
-foreach ($TypeName in $SecondCmdParamThatPipe) {
-  $pipeingParam = $TypeName.Name
-  if ($TypeName.type.name -match $FirstObjectType) {$ByVal = $true; break}
+$SecondCmdParamThatPipeByPN = (get-help -full $SecondCommand).parameters.parameter | 
+  Select-Object -Property Name,PipelineInput,type | 
+  Where-Object {$_.pipelineinput -like '*byPropertyName*'}  
+foreach ($TypeName in $SecondCmdParamThatPipeByVal) {
+  $PipeingByValParam = $TypeName.Name
+  if ($TypeName.type.name -match $FirstCmdObjectType) {$ByVal = $true; break}
 }
 # Try ByValue logic first
 If ($ByVal -eq $true) {
   Write-host -ForegroundColor Yellow -NoNewline "$FirstCommand `| $SecondCommand "
   Write-Host -NoNewline "will pipeline data "
   Write-Host -ForegroundColor green "ByValue"
-  Write-Host -NoNewline "The first command created an object type of "
-  Write-Host -ForegroundColor green $FirstObjectType
+  Write-Host -NoNewline "The first command created an object of type "
+  Write-Host -ForegroundColor green $FirstCmdObjectType
   Write-host -NoNewline "The second command can accept "
-  write-host -NoNewline -ForegroundColor Green $FirstObjectType
-  Write-Host -NoNewline " ByValue with parameter "
-  Write-Host -ForegroundColor Green $PipeingParam
+  write-host -NoNewline -ForegroundColor Green $FirstCmdObjectType
+  Write-Host -NoNewline " ByValue via the parameter "
+  Write-Host -ForegroundColor Green $PipeingByValParam
 }
 else {
   
+
 }
 
 # Test pipeline by PropertyName if Value failed
