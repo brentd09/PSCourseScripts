8a5fdc9b56630aef529fa9b2a89558778bbc21e6
Brent Denny <brent.denny@ddls.com.au>
2019-05-09 14:50:51 +1000

    Array test needed some serious tidying

--- ArrayTools/Test-ArrayEqualOrSubnet.ps1
+++ ArrayTools/Test-ArrayEqualOrSubnet.ps1
@@ -1,11 +1,27 @@
-﻿$A = 1,2,3,5
-$B = 1,2,3,4
+﻿<#
+.SYNOPSIS
+  This tests to see if one array is equal to or a subset of another array
+.DESCRIPTION
+  This tests to see if one array is equal to or a subset of another array
+.EXAMPLE
+  Test-ArrayEqualorSubset -Array1 1,2,3 -Array2 1,2
+  This will test which arrays are equal or subsets of the other
+.NOTES
+  General notes
+  Created By: Brent Denny
+  Created On: Long time ago
+  Cleaned UP: 9-May-2019
+#>
+[CmdletBinding()]
+Param (
+  [int[]]$Array1 = @(1,2,3,5),
+  [int[]]$Array2 = @(1,2,3)
+)
+$A1A2 = $Array1 | Where-Object {$_ -notin $Array2}
+$A2A1 = $Array2 | Where-Object {$_ -notin $Array1}
 
-$AB = $A | where {$_ -notin $B}
-$BA = $B | where {$_ -notin $A}
-
-if     ($AB -eq $null -and $BA -eq $null) {'$A and $B arrays are the same'}
-elseif ($AB -eq $null -and $BA -ne $null) {'$A is a subset of $B'}
-elseif ($AB -ne $null -and $BA -eq $null) {'$B is a subset of $A'}
-elseif ($AB -ne $null -and $BA -ne $null) {'$A and $B are not equal or subsets of each other'}
+if     ($A1A2.Count -eq 0 -and $A2A1.Count -eq 0) {'Array1 and Array2 arrays are the same'}
+elseif ($A1A2.Count -eq 0 -and $A2A1.Count -ne 0) {'Array1 is a subset of Array2'}
+elseif ($A1A2.Count -ne 0 -and $A2A1.Count -eq 0) {'Array2 is a subset of Aarray1'}
+elseif ($A1A2.Count -ne 0 -and $A2A1.Count -ne 0) {'Array1 and Array2 are not equal or subsets of each other'}
 
