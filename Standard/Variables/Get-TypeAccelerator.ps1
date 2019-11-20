18000e66b9295f38b074ed9fff92487d287961df
Brent Denny <brent.denny@ddls.com.au>
2019-11-20 12:39:57 +1000

    new type accel scropt

---
+++ Standard/Variables/Get-TypeAccelerator.ps1
@@ -0,0 +1,3 @@
+﻿$TypeAcceleratorKeys = ([PSObject].Assembly.GetType('System.Management.Automation.TypeAccelerators')::Get).keys 
+$TypeAcceleratorNames = New-Object -TypeName psobject -Property @{TypeAccel = $TypeAcceleratorKeys} |  Sort-Object  -Property TypeAccel
+$TypeAcceleratorNames.TypeAccel | Sort-Object 
