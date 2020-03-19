8b28fcc23712eb373832f013b85df3db2bfd7e03
Brent Denny <brent.denny@ddls.com.au>
2020-03-19 16:28:19 +1000

    changes to roomreport

--- Advanced/Strings/RoomReport.ps1
+++ Advanced/Strings/RoomReport.ps1
@@ -18,21 +18,19 @@ Param (
   [string]$FilePath = 'c:\qld.csv'
 )
   
-$RoomReport = Import-Csv $FilePath | Select-Object -Property CourseName,
-                                                             Duration,
-                                                             Type,
-                                                             Instructor,
-                                                             Room,
-                                                             @{n='StartDate';e={$_.StartDate -as [datetime]}},
-                                                             @{n='EndDate';e={$_.EndDate -as [datetime]}},
-                                                             MaxStudents,
-                                                             NumBoookings,
-                                                             RemoteStudents,
-                                                             RemoteBookings1,
-                                                             TP_Type,
-                                                             StatusName
-$RoomReport
-$CurrentDate = Get-Date
+$RoomReport = Import-Csv $FilePath | Select-Object -Property   Room,
+  CourseName, 
+  @{n='Duration';e={$_.Duration -as [int]}},
+  @{n='Trainer';e={if ($_.Instructor -ne 'Not Assigned'){$_.Instructor -replace '^(\w+)\s+(\w)\w+','$1$2'}else{'NA'}}},
+  @{n='StartDate';e={$_.StartDate -as [datetime]}},
+  @{n='EndDate';e={$_.EndDate -as [datetime]}},
+  @{n='Bookings';e={"$($_.NumBookings)L $($_.RemoteStudents)T $($_.RemoteBookings1)Z"}}
+$CurrentDate = Get-Date -Hour 0 -Minute 0 -Second 0
 $MondayCorrection = 0 - ($CurrentDate.DayOfWeek.value__ - 1)
-$MondayDate = $CurrentDate.AddDays($MondayCorrection)
-$FridayDate = $MondayDate.AddDays(4)
+$MondayDate = ($CurrentDate.AddDays($MondayCorrection)).AddSeconds(-1)
+$FridayDate = ($MondayDate.AddDays(4)).AddSeconds(1)
+$RoomReport  | 
+ Where-Object {$_.StartDate -ge $MondayDate -and $_.StartDate -le $FridayDate }  | 
+ Sort-Object -Property Room,StartDate |
+ Select-Object -Property *,@{n='Start';e={$_.StartDate.toShortDateString()}},@{n='End';e={$_.EndDate.toShortDateString()}}  -ExcludeProperty StartDate,EndDate |
+ Format-Table
