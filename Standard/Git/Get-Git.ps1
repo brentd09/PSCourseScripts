a91e1553d7bd41e8fcc7bc42fd39327b4098d308
Brent Denny <brent.dennny@ddls.com.au>
2020-10-26 23:42:57 +1000

    git tools

---
+++ Standard/Git/Get-Git.ps1
@@ -0,0 +1,7 @@
+﻿$gitpage = Invoke-WebRequest -Uri 'https://git-scm.com/download/win' -UseBasicParsing 
+$latestlink = ($gitpage.links | Where-Object {$_ -match '64-bit Git for Windows Setup'}) -replace '^.+href="(https.+exe)".+$','$1'
+$Gitexe = $latestlink -replace 'https.+\/','' 
+
+$web = [System.Net.WebClient]::new()
+$web.DownloadFile($latestlink,$Gitexe)
+
