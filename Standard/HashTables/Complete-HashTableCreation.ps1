14ba98a08a9a6e0bff0faa78603e962444b434e6
brent denny <brent.denny@ddls.com.au>
2020-10-29 12:06:57 +1000

    added type to hash table assignments

--- Standard/HashTables/Complete-HashTableCreation.ps1
+++ Standard/HashTables/Complete-HashTableCreation.ps1
@@ -8,26 +8,26 @@ $HashTable1
 
 # Creating hash tables method 2
 $HashTable2 = [ordered]@{
-  IPAddress = '192.168.2.100'
-  SubnetMask = '255.255.255.0'
-  DefaultGW = '192.168.2.1'
+  IPAddress = [ipaddress]'192.168.2.100'
+  SubnetMask = [ipaddress]'255.255.255.0'
+  DefaultGW = [ipaddress]'192.168.2.1'
 }
 
 $HashTable2
 
 # Creating hash tables method 3
 $HashTable3 = [ordered]@{}
-$HashTable3.Add('IPAddress','192.168.3.100')
-$HashTable3.Add('SubnetMask','255.255.255.0')
-$HashTable3.Add('DefaultGW','192.168.3.1')
+$HashTable3.Add('IPAddress',[ipaddress]'192.168.3.100')
+$HashTable3.Add('SubnetMask',[ipaddress]'255.255.255.0')
+$HashTable3.Add('DefaultGW',[ipaddress]'192.168.3.1')
 
 $HashTable3
 
 # Using HashTables to create Objects
 
 $HashTable4 = @{
-  Name = 'Harry'
-  Department = 'Sales'
+  Name = [string]'Harry'
+  Department = [string]'Sales'
   DateStarted = [datetime]'4 Jan 1993'
 }
 $Employee = New-Object -TypeName psobject -Property $HashTable4
