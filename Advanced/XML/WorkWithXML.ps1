30c7e8d5564461e23f8386d884030818b8b497cd
brent denny <brent.denny@ddls.com.au>
2021-01-25 13:02:40 +1000

    Updates to XML script and sample

--- Advanced/XML/WorkWithXML.ps1
+++ Advanced/XML/WorkWithXML.ps1
@@ -1,20 +1,41 @@
-﻿[xml]$Catalog = get-content .\Books-sample.xml
+﻿<#
+  This is another way of loading xml documents
+    $Catalog = New-Object -TypeName xml
+    $Catalog.Load('.\Books-sample.xml')
+#>
+
+
+[xml]$Catalog = get-content .\Books-sample.xml
 
 ### XPATH QUERIES
 # / - search from current position in the DOM
-$Catalog.SelectNodes("/catalog/book[price]")
-$Catalog.SelectNodes("/catalog/book[author='Galos, Mike']")
+# Take note of the structure when lookin at the results
+# - Notice the difference between the attibute "id" and the elements "author, title, genre..."
+$Catalog.SelectNodes("/bookshelf/book[price>30]")
+$Catalog.SelectNodes("/bookshelf/book[author='Galos, Mike']")
 
+# Notice that searching in XML is case sensitive, this yeild no results
+$Catalog.SelectNodes("/bookshelf/book[author='galos, mike']")
+
+# This will get the last node in the current bookshelf so we can increment the id number
+# for the new book that is just about to be created
+$LastBook = $Catalog.SelectSingleNode('/bookshelf/book[last()]')
+$LastIdNumber = ($LastBook.id -replace '[^0-9]','') -as [int]
+
+
+
+## This section shows us how to create a new node, attribute and sub elements
 # Create new Book
-$parent = $Catalog.SelectSingleNode('/catalog')
+$parent = $Catalog.SelectSingleNode('/bookshelf')
 $child = $Catalog.CreateElement('element','book','')
 $parent.AppendChild($child)
 
 # Find New Book
-$NewBook = $Catalog.SelectSingleNode('/catalog/book[last()]')  
+# This located the book element that we just created in the previous code block
+$NewBook = $Catalog.SelectSingleNode('/bookshelf/book[last()]')  
 
 # Add structure 
-$id      = $Catalog.CreateElement('element','id','')
+# Note the structure does not include the attribute "id"
 $author  = $Catalog.CreateElement('element','author','')
 $title   = $Catalog.CreateElement('element','title','')
 $genre   = $Catalog.CreateElement('element','genre','')
@@ -23,7 +44,7 @@ $PubDate = $Catalog.CreateElement('element','publish_date','')
 $Desc    = $Catalog.CreateElement('element','description','')
 
 # Append a strucure to the new book
-$NewBook.AppendChild($id)
+# Note that we do not add the "id" attribute here, as these are elements
 $NewBook.AppendChild($author)
 $NewBook.AppendChild($title)
 $NewBook.AppendChild($genre)
@@ -31,14 +52,18 @@ $NewBook.AppendChild($price)
 $NewBook.AppendChild($PubDate)
 $NewBook.AppendChild($Desc)
 
-# Populate the last empty book info with data
-$Catalog.SelectSingleNode('/catalog/book[last()]').id = 'bk113'
-$Catalog.SelectSingleNode('/catalog/book[last()]').author = 'Denny, Brent'
-$Catalog.SelectSingleNode('/catalog/book[last()]').title = 'The Life of a scripter'
-$Catalog.SelectSingleNode('/catalog/book[last()]').genre = 'IT'
-$Catalog.SelectSingleNode('/catalog/book[last()]').price = '10.34'
-$Catalog.SelectSingleNode('/catalog/book[last()]').publish_date = '2018-01-22'
-$Catalog.SelectSingleNode('/catalog/book[last()]').Description = 'A really boring book'
+$LastBook = $Catalog.SelectSingleNode('/bookshelf/book[last()]')
+
+# Populate the new "empty" book info with data
+# Notice how the attribute "id" is set at the start of this code block
+$NextIdNumber = 'bk' + ($LastIdNumber + 1)
+$LastBook.SetAttribute('id', $NextIdNumber)
+$LastBook.author = 'Denny, Brent'
+$LastBook.title = 'The Life of a scripter'
+$LastBook.genre = 'IT'
+$LastBook.price = '10.34'
+$LastBook.publish_date = '2018-01-22'
+$LastBook.Description = 'How to get things done while you are asleep. Getting your weekends back from the clutches of IT support'
 
 # Save the modified XML data to a file
-$Catalog.Save("C:\Users\Administrator\Documents\newbooks.xml")
+$Catalog.Save(".\UpdatedBookList.xml")
