567000af516a3c1c503bba1a7411508a02838d33
brent denny <brent.denny@ddls.com.au>
2021-05-03 23:08:24 +1000

    show sub properties

---
+++ Standard/Pipelines/Show-AllProperties.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+  Short description
+.DESCRIPTION
+  Long description
+.EXAMPLE
+  PS C:\> <example usage>
+  Explanation of what the example does
+.INPUTS
+  Inputs (if any)
+.OUTPUTS
+  Output (if any)
+.NOTES
+  General notes
+#>
+[CmdletBinding()]
+Param (
+  [Parameter(ValueFromPipeline)]
+  [psobject[]]$InputObject
+)
+Begin {}
+Process {
+  foreach ($Object in $InputObject) {
+    
+  }
+}
+End {}
