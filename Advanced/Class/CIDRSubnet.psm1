971f31c87bf1e6e667eb23cc14695f6116a1e018
brent denny <brent.denny@ddls.com.au>
2021-06-28 14:56:24 +1000

    new scripts

---
+++ Advanced/Class/CIDRSubnet.psm1
@@ -0,0 +1,29 @@
+﻿Class CidrMask {
+  [int]$CidrMask
+  [string]$DotDecMask
+  [int]$SubnetsJumpBy
+  [int]$JumpAtBit  
+
+  CidrMask ([int]$MaskLength) {
+    $NumFullMaskOctets = [math]::Truncate($MaskLength/8)
+    $SubBits = $MaskLength % 8
+    $HiOrdHostBit = 8 - $SubBits
+    if ($SubBits -eq 0) {
+      $JumpBy = 1
+      $JumpBit = $NumFullMaskOctets
+    }
+    else {
+      $JumpBy = [math]::Pow(2,$HiOrdHostBit)
+      $JumpBit = $NumFullMaskOctets + 1
+    }
+    $HostBits = [math]::Pow(2,$HiOrdHostBit)-1
+    $MaskforOctet = $HostBits -bxor 255
+    $DotMask = ("255."*$NumFullMaskOctets)+$MaskforOctet+(".0"*(4-($NumFullMaskOctets+1)))  
+    $this.CidrMask      = $MaskLength
+    $this.DotDecMask    = $DotMask
+    $this.SubnetsJumpBy = $JumpBy
+    $this.JumpAtBit     = $JumpBit
+    }   
+}
+
+$subnetInfo = [CidrMask]::New(23)
---
+++ Standard/ProgressBar/Write-ProgressBars.ps1
@@ -0,0 +1,11 @@
+﻿$Outer = 1..100
+$Inner = 1..10
+
+foreach ($Out in $Outer) {
+  Write-Progress -Id 1 -Activity "Doing the Outer stuff" -PercentComplete ($Out/$Outer[-1]*100) -Status "Doing some cool stuff"
+  foreach ($In in $Inner) {
+  Write-Progress -ID 2 -ParentId 1 -Activity "Doing the Inner stuff" -PercentComplete ($In/$Inner[-1]*100) -Status "$($In/$Inner[-1]*100)% Complete"
+  1..20 | ForEach-Object {Write-Progress -ParentId 2 -Activity "Doing the Count" -PercentComplete ($_/20*100) -CurrentOperation "Current Number $_" -Status "Counting" ; Start-Sleep -Milliseconds 100 }
+  start-sleep -Milliseconds 100  
+  } 
+}
