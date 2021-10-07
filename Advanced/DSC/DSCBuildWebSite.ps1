b144c856b9c3bdf7df60eeda49ff66ddc9f75f18
brent denny <brent.denny@ddls.com.au>
2021-10-07 23:17:47 +1000

    iis dsc build

--- Advanced/DSC/DSCBuildWebSite.ps1
+++ Advanced/DSC/DSCBuildWebSite.ps1
@@ -1,1 +1,18 @@
+Configuration BuildIIS {
+  Param ([string]$ComputerName = 'LON-SVR1')
 
+  Node $ComputerName {
+    WindowsFeature IIS {
+      Name = 'Web-Server'
+      Ensure = 'Present'
+    }
+
+    WindowsFeature RSAT_IIS {
+      Name = 'Web-Mgmt-Console'
+      Ensure = 'Present'
+      DependsOn = 'IIS'
+    }
+  }
+}
+
+BuildIIS 
