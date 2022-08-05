875ee017e2c01b6b3a662ef09450cfeb4941300a
Brent Denny <brent.denny@ddls.com.au>
2022-08-06 01:14:55 +1000

    pipeline explain bypn to complete

--- Standard/Pipelines/Pipelines.ps1
+++ Standard/Pipelines/Pipelines.ps1
@@ -1,9 +1,9 @@
 [CmdletBinding()]
 Param (
-  [Parameter(Mandatory=$true)]
-  [String]$FirstCommand,
-  [Parameter(Mandatory=$true)]
-  [String]$SecondCommand
+  #[Parameter(Mandatory=$true)]
+  [String]$FirstCommand = 'Get-Service',
+  #[Parameter(Mandatory=$true)]
+  [String]$SecondCommand = 'Stop-Process'
 )
 function Get-ObjectType {
   Param (
@@ -49,8 +49,92 @@ function Get-ObjectMember {
   }
   return $PipelineParams
 }
+function Show-ByVal {
+  Param (
+    $FirstCmd,
+    $SecondCmd,
+    $FirstCmdType,
+    $SecondCmdParamByVal
+  )
+  Clear-Host
+  Write-Host -ForegroundColor Blue "ByValue Pipeline"
+  Write-Host -ForegroundColor Blue "----------------"
+  Write-Host "The first command " -NoNewline
+  Write-Host -ForegroundColor Red $FirstCmd  -NoNewline
+  Write-Host " produces an object of type " -NoNewline
+  Write-Host -ForegroundColor Red $FirstCmdType
+  Write-Host "The second command " -NoNewline
+  Write-Host -ForegroundColor Red $SecondCmd -NoNewline
+  Write-Host " accepts " -NoNewline
+  Write-Host -ForegroundColor Red $FirstCmdType -NoNewline
+  Write-Host " objects " 
+  Write-Host "being pipelined " -NoNewline
+  Write-Host -ForegroundColor Blue "ByValue" -NoNewline
+  Write-Host " via the parameter -" -NoNewline
+  Write-Host -ForegroundColor Red $SecondCmdParamByVal[0].Name
+  if ($SecondCmdParamByVal[0].Type -eq 'PSObject') {
+    Write-Host "`nPSObject is a special case in which PSObject will accept any object" 
+  }
+  Write-Host 
+  Write-Host -ForegroundColor Blue "FOR EXAMPLE"
+  Write-Host -ForegroundColor Blue "-----------"
+  Write-Host "$FirstCmd --> $FirstCmdType (Object)" -NoNewline
+  Write-Host -ForegroundColor Green "  =====>> | =====>>  " -NoNewline
+  Write-Host "-$($SecondCmdParamByVal[0].Name)[$($SecondCmdParamByVal[0].Type)] --> $SecondCmd"
+  Write-Host
+}
 
-#Get-ObjectType -Command $FirstCommand
-Get-ObjectProperty -Command $FirstCommand 
-Out-String 
-Get-ObjectMember -Command $SecondCommand -PipelineMethod ByPropertyName
+function Show-ByPropertyName {
+  Param (
+    $FirstCmd,
+    $SecondCmd,
+    $FirstCmdType,
+    $FirstCmdProperties,
+    $SecondCmdParamsByPN
+  )
+  Clear-Host
+  Write-Host -ForegroundColor Blue "ByPropertName Pipeline"
+  Write-Host -ForegroundColor Blue "----------------------"
+  Write-Host "The first command " -NoNewline
+  Write-Host -ForegroundColor Red $FirstCmd  -NoNewline
+  Write-Host " produces an object of type " -NoNewline
+  Write-Host -ForegroundColor Red $FirstCmdType
+  Write-Host "However, the second command " -NoNewline
+  Write-Host -ForegroundColor Red $SecondCmd -NoNewline
+  Write-Host " does not allow " -NoNewline
+  Write-Host -ForegroundColor Red $FirstCmdType -NoNewline
+  Write-Host " objects "
+  Write-Host "to be pipelined " -NoNewline
+  Write-Host -ForegroundColor Blue "ByValue" -NoNewline
+  Write-Host " via any of the parameters" 
+  Write-Host
+  Write-Host "Therefore the pipeline now tries to pipe " -NoNewline
+  Write-Host -ForegroundColor Blue "ByPropertyName" -NoNewline
+  Write-Host " in which property values "
+  Write-Host "from the first command are pipelined into the parameters of the second command"
+  Write-Host "if they are both spelt the same and both have the same data types"
+  Write-Host 
+  Write-Host -ForegroundColor Blue "FOR EXAMPLE"
+  Write-Host -ForegroundColor Blue "-----------"
+
+}
+
+# MAIN CODE
+
+# First lets see if ByValue will work, if not, lets see of ByPropertyName will work
+$FirstCmdOutputType = Get-ObjectType -Command $FirstCommand
+$SecondCmdParamByVal = Get-ObjectMember -Command $SecondCommand -PipelineMethod ByValue
+if ($FirstCmdOutputType -in $SecondCmdParamByVal.Type) {
+  $ByValMatchingParam = $SecondCmdParamByVal | Where-Object {$_.Type -eq $FirstCmdOutputType}
+  Show-ByVal -FirstCmd $FirstCommand -SecondCmd $SecondCommand -FirstCmdType $FirstCmdOutputType -SecondCmdParam $ByValMatchingParam
+}
+elseif ($SecondCmdParamByVal.Type -eq 'PSObject') {
+  $ByValMatchingParam = $SecondCmdParamByVal | Where-Object {$_.Type -eq 'PSObject'}
+  Show-ByVal -FirstCmd $FirstCommand -SecondCmd $SecondCommand -FirstCmdType $FirstCmdOutputType -SecondCmdParam $ByValMatchingParam
+}
+else {
+# If not, lets see of ByPropertyName will work
+$FirstCmdOutputProperties = Get-ObjectProperty -Command $FirstCommand 
+$SecondCmdParamByPN = Get-ObjectMember -Command $SecondCommand -PipelineMethod ByPropertyName
+Show-ByPropertyName -FirstCmd $FirstCommand $SecondCommand -FirstCmdType $FirstCmdOutputType -FirstCmdProperties $FirstCmdOutputProperties -SecondCmdParamsByPN $SecondCmdParamByPN
+}
