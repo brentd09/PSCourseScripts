dd3031e38d84abb774619e9115e4a8f37238e943
BrentD09 <brent.denny@ddls.com.au>
2025-06-12 10:53:19 +1000

    Update PipelineTools.psm1

--- Standard/Pipelines/PipelineTools.psm1
+++ Standard/Pipelines/PipelineTools.psm1
@@ -16,17 +16,17 @@ function Get-PipeLineParameter {
     Get-PipeLineParameter -Cmdlet Get-Process
     This will show the parameters that will accept pipeline input for the Get-Process command:
 
-    ParameterSetName        ParameterName ParameterType                ByValue ByPropertyName
-    ----------------        ------------- -------------                ------- --------------
-    Name                    Name          System.String[]                False           True
-    Name                    ComputerName  System.String[]                False           True
-    NameWithUserName        Name          System.String[]                False           True
-    IdWithUserName          Id            System.Int32[]                 False           True
-    Id                      Id            System.Int32[]                 False           True
-    Id                      ComputerName  System.String[]                False           True
-    InputObjectWithUserName InputObject   System.Diagnostics.Process[]    True          False
-    InputObject             InputObject   System.Diagnostics.Process[]    True          False
-    InputObject             ComputerName  System.String[]                False           True
+    ParameterSetName        ParameterName ByValue ByPropertyName ParameterType               
+    ----------------        ------------- ------- -------------- -------------               
+    Name                    Name            False           True System.String[]             
+    Name                    ComputerName    False           True System.String[]             
+    NameWithUserName        Name            False           True System.String[]             
+    IdWithUserName          Id              False           True System.Int32[]              
+    Id                      Id              False           True System.Int32[]              
+    Id                      ComputerName    False           True System.String[]             
+    InputObjectWithUserName InputObject      True          False System.Diagnostics.Process[]
+    InputObject             InputObject      True          False System.Diagnostics.Process[]
+    InputObject             ComputerName    False           True System.String[] 
   #>
   [cmdletbinding()]
   Param (
@@ -40,9 +40,9 @@ function Get-PipeLineParameter {
     $ReturnPipelineDetails = $PipelineParams | 
      Select-Object -Property @{n='ParameterSetName';e={$ParamSet.Name}},
                              @{n='ParameterName';e={$_.Name}},
-                             ParameterType, 
                              @{n='ByValue';e={$_.ValueFromPipeline}}, 
-                             @{n='ByPropertyName';e={$_.ValueFromPipelineByPropertyName}}
+                             @{n='ByPropertyName';e={$_.ValueFromPipelineByPropertyName}}, 
+                             ParameterType
     $ReturnPipelineDetails 
   }
 }
